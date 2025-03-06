e806e4dfb3e1fecd24bbbc9a19b43aa448d68f8f
Nduta-E <mesther@kabarak.ac.ke>
2025-03-06 17:00:56 +0300

    Removed all files but kept them locally

--- images/process_face_encoding.php
+++
@@ -1,8 +0,0 @@
-<?php
-$regno = $_GET['regno']; // Make sure to pass regno in the request
-
-$command = escapeshellcmd("python capture_face.py $regno");
-$output = shell_exec($command);
-
-echo $output;
-?>
